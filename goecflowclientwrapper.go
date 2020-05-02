7f17d7d420526a88c6276edbbfd9a9ebc5d1404a
Espen Myrland <espenm@met.no>
2020-05-02 11:24:13 +0000

    wip

--- goecflowclientwrapper.go
+++ goecflowclientwrapper.go
@@ -1,36 +1,53 @@
 package goecflowclientwrapper
 
 import (
-	"log"
+	"fmt"
 	"os"
 	"os/exec"
 )
 
+// No swig . Just call ecflow_client and get over with it.
+
 // Ecflowinit - Run ecflow_client --init ...
-func Ecflowinit() {
-	rfid := os.Getenv("JOB_ID")
+func Ecflowinit(host string, port string) error {
+
+	if os.Getenv("NO_ECF") != "" && os.Getenv("NO_ECF") != "0" {
+		return nil
+	}
+
+	rfid := fmt.Sprintf("%d", os.Getpid())
+	if os.Getenv("JOB_ID") != "" { // Running on gridengine
+		rfid = os.Getenv("JOB_ID")
+	}
+
 	cmdinit := exec.Command("ecflow_client",
-		"--host", os.Getenv("ECF_HOST"),
-		"--port", os.Getenv("ECF_PORT"),
+		"--host", host,
+		"--port", port,
 		"--init",
 		rfid,
 	)
 	stdoutStderr, errinit := cmdinit.CombinedOutput()
 	if errinit != nil {
-		log.Printf("ERROR: Command %v failed with %s, output: %s\n", cmdinit, errinit, stdoutStderr)
+		return fmt.Errorf("Ecflowinit: command %v failed with %s, output: %s",
+			cmdinit, errinit, stdoutStderr)
 	}
+	return nil
 }
 
 // Ecflowcomplete - Run ecflow_client --complete  ...
-func Ecflowcomplete() {
+func Ecflowcomplete(host string, port string) error {
+	if os.Getenv("NO_ECF") != "" && os.Getenv("NO_ECF") != "0" {
+		return nil
+	}
+
 	cmdcomplete := exec.Command("ecflow_client",
-		"--host", os.Getenv("ECF_HOST"),
-		"--port", os.Getenv("ECF_PORT"),
+		"--host", host,
+		"--port", port,
 		"--complete",
 	)
 	stdoutStderr, errcomplete := cmdcomplete.CombinedOutput()
 	if errcomplete != nil {
-		log.Printf("ERROR: Command %v failed with %s, output: %s\n", cmdcomplete, errcomplete, stdoutStderr)
+		return fmt.Errorf("Ecflowcomplete: Command %v failed with %s, output: %s", cmdcomplete, errcomplete, stdoutStderr)
 	}
-
+	return nil
 }
