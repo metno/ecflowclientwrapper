cfc31c623677175892a5c815aea8dfec5a8fdd3f
Espen Myrland <espenm@met.no>
2021-04-20 12:18:47 +0000

    ecflow_client path fix

--- goecflowclientwrapper.go
+++ goecflowclientwrapper.go
@@ -5,9 +5,18 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"log"
 )
 
 // No swig . Just call ecflow_client and get over with it.
+var ecflow_client_bin string
+
+func init() {
+	if os.Getenv("ECFLOW_CLIENT_BIN") == "" {
+		log.Fatalf("Env var ECFLOW_CLIENT_BIN empty")
+	}
+	ecflow_client_bin = os.Getenv("ECFLOW_CLIENT_BIN")
+}
 
 // Ecflowinit - Run ecflow_client --init ... Use env JOB_ID for rfid if set (=> Running on gridengine)
 func Ecflowinit(host string, port string) error {
@@ -37,8 +46,8 @@ func Ecflowcomplete(host string, port string) error {
 
 // EcflowClient - Call ecflow_client binary. Returns teh command stdout, stderr, and go's cmd.Run() error
 func EcflowClient(args ...string) (string, string, error) {
-
-	cmd := exec.Command("ecflow_client", args...)
+	
+	cmd := exec.Command(ecflow_client_bin, args...)
 	var stdout, stderr bytes.Buffer
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
